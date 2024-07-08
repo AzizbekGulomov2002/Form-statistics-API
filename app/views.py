aca322ce40ede0bc882fb6480dd4b35a2dac2214
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-07-09 00:20:22 +0500

    statistics api null = 0

--- app/views.py
+++ app/views.py
@@ -18,38 +18,51 @@ class SurveyViewSet(viewsets.ModelViewSet):
 
 
 
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Count
+from .models import Survey
+from .serializers import SurveySerializer, StatisticsSerializer
+
+class SurveyViewSet(viewsets.ModelViewSet):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+
 class StatisticsView(APIView):
     def get(self, request):
-        ecommerce_stats = Survey.objects.values('ecommerce_usage').annotate(count=Count('id'))
-        turnover_stats = Survey.objects.values('monthly_turnover').annotate(count=Count('id'))
-        commission_stats = Survey.objects.values('commission_requirement').annotate(count=Count('id'))
-        
+        ecommerce_stats = dict(Survey.objects.values('ecommerce_usage').annotate(count=Count('id')).values_list('ecommerce_usage', 'count'))
+        turnover_stats = dict(Survey.objects.values('monthly_turnover').annotate(count=Count('id')).values_list('monthly_turnover', 'count'))
+        commission_stats = dict(Survey.objects.values('commission_requirement').annotate(count=Count('id')).values_list('commission_requirement', 'count'))
+
         ecommerce_mapping = dict(Survey.ECOMMERCE_CHOICES)
         turnover_mapping = dict(Survey.TURNOVER_CHOICES)
         commission_mapping = dict(Survey.COMMISSION_CHOICES)
-        
+
         formatted_stats = {
             'ecommerce_usage': [
                 {
-                    'platform': ecommerce_mapping.get(stat['ecommerce_usage'], 'Unknown'),
-                    'count': stat['count']
+                    'platform': ecommerce_mapping.get(choice[0], 'Unknown'),
+                    'count': ecommerce_stats.get(choice[0], 0)
                 }
-                for stat in ecommerce_stats
+                for choice in Survey.ECOMMERCE_CHOICES
             ],
             'monthly_turnover': [
                 {
-                    'turnover': turnover_mapping.get(stat['monthly_turnover'], 'Unknown'),
-                    'count': stat['count']
+                    'turnover': turnover_mapping.get(choice[0], 'Unknown'),
+                    'count': turnover_stats.get(choice[0], 0)
                 }
-                for stat in turnover_stats
+                for choice in Survey.TURNOVER_CHOICES
             ],
             'commission_requirement': [
                 {
-                    'commission': commission_mapping.get(stat['commission_requirement'], 'Unknown'),
-                    'count': stat['count']
+                    'commission': commission_mapping.get(choice[0], 'Unknown'),
+                    'count': commission_stats.get(choice[0], 0)
                 }
-                for stat in commission_stats
+                for choice in Survey.COMMISSION_CHOICES
             ]
         }
-        
+
         return Response(formatted_stats, status=status.HTTP_200_OK)
+
